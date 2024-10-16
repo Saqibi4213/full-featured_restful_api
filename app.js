63952afc8d5ca6639117d31f47feafc758c094d1
Saqibi4213 <mehriamosawer@gmail.com>
2024-10-16 20:02:13 +0430

    error handling app.js

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const helmet = require('helmet');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const bookRoutes = require('./routes/bookRoutes');
-// const favicon = require('serve-favicon'); // Remove this line
-const path = require('path');
 
 dotenv.config();
 
@@ -14,20 +12,21 @@ connectDB();
 
 const app = express();
 
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); // Remove or comment this line
+// Middleware
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 
-
+// API routes
 app.use('/api/books', bookRoutes);
 
-
+// Default route
 app.get('/', (req, res) => {
   res.send('Welcome to the Full-Featured RESTful API');
 });
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
